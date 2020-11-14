f650b4dc8c136e18d183e72207255bac5de3447d
Salome Kuitca <salokuitca@gmail.com>
2020-11-14 11:31:38 -0300

    Primera version sin carrito

---
+++ script.js
@@ -0,0 +1,7 @@
+$('.navbar-nav>li>a').on('click', function () {
+    $('.navbar-collapse').collapse('hide');
+});
+
+function promo() {
+    $('#descripcion-dire').val('Promo Aplicada: "Piquibola" \n -----------------------------\n')
+}
